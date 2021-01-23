e1736ea135dca3bea15e100f67f238d44fb35a54
Robert Hung Nguyen <nthung2411@gmail.com>
2021-01-23 11:00:32 +0700

    add TodoItem api with in memory database

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -19,7 +21,7 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
